ba3b5fcd2e06d4162f6e7efcc4f63facf0af005e
Abu Ashraf Masnun <masnun@gmail.com>
2011-11-11 04:28:25 +0600

    Added JS based loading and language hinting

--- avro-phonetic.php
+++ avro-phonetic.php
@@ -15,37 +15,82 @@
 
 add_action('admin_head', 'avro_phonetic');
 add_action('wp_head', 'avro_phonetic');
+add_action('wp_footer', 'avro_phonetic_notif');
+add_action('admin_footer', 'avro_phonetic_notif');
+
+function avro_phonetic_notif()
+{
+    echo '<div id="avro-phonetic-notif">E</div>';
+}
 
 function avro_phonetic()
 {
-    
     ?>
-    <script src="http://code.jquery.com/jquery-1.7.min.js" type="text/javascript" charset="utf-8"></script>
-    <script src="https://raw.github.com/torifat/jsAvroPhonetic/1.0-beta/src/avro-1.0-beta.min.js" type="text/javascript" charset="utf-8"></script>
-    <script type="text/javascript" charset="utf-8">
-        jQuery(function(){
-            alertOn = false
-            jQuery('textarea, input[type=text]').avro({'bn':false},
-            function(isBangla){
-                if(isBangla) {
-                    if(alertOn) {
-                        alert("Keyboard Switched to Bangla")
-                    } else {
-                        alertOn = true
-                    }
-                } else {
-                    if(alertOn) {
-                        alert("Keyboard Switched to English")
-                    } else {
-                        alertOn = true
-                    }
-                }
-            });
-        });
 
+    <style type="text/css">
+        #avro-phonetic-notif { 
+            border: 0;
+            position: fixed;
+            top: 200px; 
+            right:0;
+            width: 50px;
+            height: 50px;
+            background-color: #000;
+            color: #ffff33;
+            font-weight: bold;
+            text-align: center;
+            padding-top: 25px; 
+            
+        }
+    </style>
+
+
+    <script type="text/javascript">
+                        
+                            
+        var root = (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]);
+        var ns = document.createElementNS && document.documentElement.namespaceURI;
+
+        if(typeof jQuery === 'undefined') {
+            var script = ns ? document.createElementNS(ns, 'script') : document.createElement('script');
+            script.type = 'text/javascript';
+            script.onreadystatechange = function () {
+                if (this.readyState == 'complete') enable_avro();
+            }
+            script.onload= enable_avro;
+            script.src= 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.min.js';
+            root.appendChild(script);
+        } else {
+            enable_avro();
+        }
+
+        function enable_avro() {
+            jQuery.noConflict();
+            var script = ns ? document.createElementNS(ns, 'script') : document.createElement('script');
+            script.type = 'text/javascript';
+            script.onreadystatechange = function () {
+                if (this.readyState == 'complete') avro_js_loader();
+            }
+            script.onload= avro_js_loader;
+            script.src= 'https://raw.github.com/torifat/jsAvroPhonetic/master/src/avro-latest.js';
+            root.appendChild(script);
+                                
+        }
+
+        function avro_js_loader() {
+            jQuery('textarea, input[type=text]').live('focus', function() {
+                jQuery(this).avro('destroy').avro({'bn':false}, function(isBangla){
+                    if(isBangla) {
+                        jQuery("#avro-phonetic-notif").html("অ")   
+                    } else {
+                        jQuery("#avro-phonetic-notif").html("E")           
+                    }
+                });
+            }).avro('destroy').avro();
+        }
+
+                                
     </script>
-
-
     <?
 
 }
